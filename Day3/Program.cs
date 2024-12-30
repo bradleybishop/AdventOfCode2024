224353a9af22daa5241c5b2e334b8316e265d9d3
Brad Bishop <bishop.bradley@gmail.com>
2024-12-29 19:36:31 -0600

    Day 3 part 1 complete

---
+++ Day3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Program();
+        p.Run();
+    }
+
+    private void Run()
+    {
+        Console.WriteLine("Day 3 of Advent of Code!");
+
+        var inputFile = "day3.txt";
+
+        var input = File.ReadAllText(inputFile);
+
+        Regex regex = new Regex(@"mul\(\d*,\d*\)",RegexOptions.IgnoreCase);
+
+        var matches = regex.Matches(input);
+
+        int total = 0;
+
+        foreach(var match in matches)
+        {
+            List<int> numbers = match.ToString().Replace("mul(","").Replace(")", "").Split(',').Select(x => int.Parse(x)).ToList();
+
+            total += numbers[0] * numbers[1];
+        }
+
+        Console.WriteLine($"Total: {total}");
+    }
+}
